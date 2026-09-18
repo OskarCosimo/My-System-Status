761b3be52fded331dc58a3911a15f0ecfdfc3665
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-18 02:13:55 +0200

    Update version from 1.0.44 to 1.0.45

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
      * The version string is checked against GitHub API tags for auto-updates.
      */
     'name'        => 'My System Status',
-    'version'     => '1.0.44',
+    'version'     => '1.0.45',
 
     /*
      * Debug Mode
