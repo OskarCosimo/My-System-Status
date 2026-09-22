634f74fd6b249ca66917256601af9c7f47c1c3f7
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-22 19:06:05 +0200

    Update version from 1.0.50 to 1.0.51

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
      * The version string is checked against GitHub API tags for auto-updates.
      */
     'name'        => 'My System Status',
-    'version'     => '1.0.50',
+    'version'     => '1.0.51',
 
     /*
      * Debug Mode
