786f72d5c6c18b09a710f295c29a655f1bebc9da
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-22 19:05:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Service Worker registration requirement for PWA installation
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Fetch handler required by browser PWA install criteria
+});
